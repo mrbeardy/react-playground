76760fe5590aa83c73288cd192c8616b7b662860
Michael Hibbs <hibbs.michael@gmail.com>
2015-03-07 23:35:09 +0000

    [hangman] Initial Commit.

---
+++ playground/hangman/assets/js/app.js
@@ -0,0 +1,154 @@
+var hangman = {};
+
+(function() {
+  var app = {}, hangman = app,
+  Application;
+
+  Application = React.createClass({displayName: "Application",
+    GAME_STATES: {
+      PLAYING:  0x01,
+      WON:      0x02,
+      LOST:     0x04
+    },
+    
+    LETTERS: "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(''),
+
+    getInitialState: function() {
+      var state = {
+        word: [],
+        words: [],
+        lettersChosen: [],
+        gameState: this.GAME_STATES.PLAYING
+      }
+
+      return state;      
+    },
+
+    componentDidMount: function() {
+      var state = this.state;
+
+      if (this.props.words)
+        state.words = this.props.words;
+
+      this.newGame();
+      
+      this.setState( state );
+    },
+
+    randomword: function() {
+      return this.state.words[_.random(0, this.state.words.length - 1)];
+    },
+
+    endGame: function( gameState ) {
+      var state = this.state;
+
+      state.gameState = gameState;
+
+      this.setState(state);
+    },
+
+    newGame: function() {
+      var state = this.state;
+
+      state.word = this.randomword().split('');
+      state.lettersChosen = [];
+      state.gameState = this.GAME_STATES.PLAYING;
+
+      this.setState(state);
+    },
+
+    hasSolved: function() {
+      var remaining = 0;
+
+      _.each(this.state.word, function(c) {
+        var upper = c.toUpperCase();
+
+        var isChosen = this.state.lettersChosen.indexOf( upper ) > -1;
+        var isLetter = this.LETTERS.indexOf( upper ) > -1;
+
+        if (!isChosen && isLetter) {
+          remaining++;
+        }
+      }, this);
+
+      return (remaining == 0);
+    },
+
+    chooseLetter: function( e ) {
+      var target = e.currentTarget,
+          letter = target.innerHTML;
+
+      if ( target.hasAttribute("disabled") ) return;
+
+      this.state.lettersChosen.push(letter);
+      this.setState({ lettersChosen: this.state.lettersChosen });
+      
+      if (this.hasSolved()) {
+        this.endGame( this.GAME_STATES.WON );
+      }
+    },
+
+    render: function() {
+      var gameState = this.state.gameState;
+      var isGameEnded = gameState == this.GAME_STATES.WON || gameState == this.GAME_STATES.LOST;
+      var didWin = gameState == this.GAME_STATES.WON;
+      var didLose = gameState == this.GAME_STATES.LOST;
+
+      if (this.state.word.length > 0) {
+        var letterItems = _.map(this.state.word, function(c, key) {
+          var upper = c.toUpperCase();
+
+          var isChosen = this.state.lettersChosen.indexOf( upper ) > -1;
+          var isNonLetter = this.LETTERS.indexOf( upper ) == -1;
+
+          return (
+            React.createElement("span", {className:  !isNonLetter ? "letter" : "", key: key}, 
+               (isGameEnded || (isChosen || isNonLetter)) ? c : "_"
+            )
+          );
+        }, this);
+      }
+
+      var buttons = _.map(this.LETTERS, function(letter) {
+        var isChosen = this.state.lettersChosen.indexOf(letter) > -1;
+
+        return (
+          React.createElement("button", {
+            key: letter, 
+            onClick:  this.chooseLetter, 
+            disabled:  ( isChosen || isGameEnded ) ? "disabled" : ""
+          }, 
+            letter 
+          )
+        );
+      }, this);
+
+      var endMessage = "";
+
+      if (isGameEnded) {
+        endMessage = (didWin) ? "Congratulations!" : "Better luck next time.";
+      }
+
+      return (
+        React.createElement("main", {id: "app"}, 
+          React.createElement("div", {className: "word"}, letterItems ), 
+          React.createElement("div", {className: "buttons"}, 
+            buttons 
+          ), 
+
+          React.createElement("div", {className: "controls"}, 
+            React.createElement("div", {className: "endMessage"}, 
+              endMessage 
+            ), 
+            React.createElement("button", {onClick:  this.newGame},  (isGameEnded) ? "Play Again" : "New Game")
+          )
+        )
+      );
+    }
+  });
+
+  app.render = function() {
+    React.render(React.createElement(Application, {words: wordlist }), document.body);
+  }
+  app.render();
+})();

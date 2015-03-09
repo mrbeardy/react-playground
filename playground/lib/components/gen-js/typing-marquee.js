98512062bbb112794b0775d95ec74bfc9bd1255b
Michael Hibbs <hibbs.michael@gmail.com>
2015-03-09 02:01:17 +0000

    [components] Add typing-marquee component.
    The /lib folder should contain common elements that a lot of the
    examples in the playground are using.
    
    The /lib/components folder should contain any re-usable react
    components such as UI elements and mixins. For the most part,
    these components aren't going to be serious things. They're
    mostly just me playing around with re-usable react components.

---
+++ playground/lib/components/gen-js/typing-marquee.js
@@ -0,0 +1,103 @@
+var TypingMarquee = React.createClass({displayName: "TypingMarquee",
+  propTypes: {
+    messages: React.PropTypes.array.isRequired,
+
+    cursorCharacter: React.PropTypes.string,
+
+    keyDelay:     React.PropTypes.number,
+    messageDelay: React.PropTypes.number,
+
+    style: React.PropTypes.object,
+
+    keyChangeCallback:      React.PropTypes.func,
+    messageChangeCallback:  React.PropTypes.func
+  },
+
+  getDefaultProps: function() {
+    return {
+      cursorCharacter: "|",
+
+      keyDelay: 90,
+      messageDelay: 3000,
+
+      style: {}
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      currentCharacterIndex: 0,
+      currentMessageIndex: 0
+    }
+  },
+
+  componentWillMount: function() {
+    this.keyInterval = null;
+    this.messageTimeout = null;
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.keyInterval);
+    clearTimeout(this.messageTimeout);
+  },
+
+  componentDidMount: function() {
+    // Refactor: Move these set/clear calls into seperate functions 
+    //           to reduce code rewrite.
+    this.keyInterval = setInterval(this.keyChange, this.props.keyDelay);
+  },
+
+  keyChange: function() {
+    var state = this.state;
+    var props = this.props;
+
+    var currentMessage = props.messages[state.currentMessageIndex];
+
+    state.currentCharacterIndex += 1;
+
+    if (state.currentCharacterIndex >= currentMessage.length) {
+      state.currentCharacterIndex = currentMessage.length;
+
+      clearInterval(this.keyInterval);
+
+      this.messageTimeout = setTimeout(this.messageChange, props.messageDelay);
+    }
+    
+    props.keyChangeCallback({
+      message: currentMessage,
+      characterIndex: state.currentCharacterIndex
+    });
+
+    this.setState(state);
+  },
+
+  messageChange: function() {
+    var state = this.state;
+    var props = this.props;
+
+    state.currentCharacterIndex = 0;
+    state.currentMessageIndex += 1;
+
+    if (state.currentMessageIndex > props.messages.length - 1) {
+      state.currentMessageIndex = 0;
+    }
+
+    this.keyInterval = setInterval(this.keyChange, this.props.keyDelay);
+
+    this.setState(state);
+  },
+
+  render: function() {
+    var state = this.state;
+    var props = this.props;
+    var currentMessage = props.messages[state.currentMessageIndex];
+
+    var message = currentMessage.substr(0, state.currentCharacterIndex);
+
+    return (
+      React.createElement("span", {style: props.style}, message, React.createElement("span", {className: "cursor"}, props.cursorCharacter))
+    )
+  }
+});
+
+module.exports = TypingMarquee;
